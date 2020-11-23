bfdd288e649a226af8330ff149b2368fc0209b4f
brooketran <btran1@stanford.edu>
2020-11-23 00:49:34 -0500

    Update map panel.R

--- R/map panel.R
+++ R/map panel.R
@@ -83,7 +83,7 @@ make_map_panel <- function(data,
     ggmap(gmap_oak) +
     geom_sf(
       data = data,
-      aes(fill = !! sym(var)),
+      aes(fill = {{var}}),
       size = 0,
       alpha = 0.5,
       inherit.aes = FALSE
