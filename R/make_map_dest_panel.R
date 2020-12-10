38936536ef26e1847b288def15fad3bafc1341ee
Vineet Gupta <55761250+VineetG11@users.noreply.github.com>
2020-12-09 21:18:35 -0800

    remove rename periods as they are already appropriately named

--- R/make_map_dest_panel.R
+++ R/make_map_dest_panel.R
@@ -69,7 +69,7 @@ make_dest_map_panel <- function(
            outmigration_alameda,
            outmigration_contracosta,
            outmigration_sanfran) %>%
-    filter(year %in% c("boom", "bust", "recovery", "post_recovery")) %>%
+    filter(year %in% c("Boom", "Bust", "Recovery", "Post-Recovery")) %>%
     filter(cat == "Overall") %>%
     select(-cat) %>%
     group_by(year) %>%
@@ -81,11 +81,6 @@ make_dest_map_panel <- function(
                                    "outmigration_contracosta" = "Contra Costa city",
                                    "outmigration_sanfran" = "San Francisco city"))
 
-  data$year = plyr::revalue(data$year,
-                            c("boom"="Boom",
-                              "bust"="Bust",
-                              "recovery"="Recovery",
-                              "post_recovery"="Post-Recovery"))
   data = data %>%
     mutate(city = variable)
 
