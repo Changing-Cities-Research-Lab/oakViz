f8809994599a6b54cf37bf1ed550abc75195ee96
brooketran <btran1@stanford.edu>
2020-12-11 11:01:19 -0500

    added ses parameter to display ses facet cols

--- R/lollipop plot.R
+++ R/lollipop plot.R
@@ -8,6 +8,7 @@
 #' @param data Data with column for variable of interest.
 #' @param var Column name of variable of interest.
 #' @param limits Y-axis limits.
+#' @param ses Displays SES facet columns when T (default).
 #' @param reverse Reverses direction of x-axis when T, default is F.
 #' @param x_title Title to display along x-axis
 #' @param scale_type Y-axis scale type: "numeric" or "percent"
@@ -21,7 +22,7 @@ plot_lollipop <- function(
   data,
   var,
   limits,
-  reverse = F,
+  ses = T,
   x_title = "",
   scale_type = "numeric",
   save = F,
@@ -34,17 +35,26 @@ plot_lollipop <- function(
   gent_cat_colors <-
     c("snow3","#d94801", "#fa7b00", "#fdcc8a", "#a6d894")
   gent_cat <- c("Nongentrifiable", "Intense", "Moderate", "Weak", "People or Price")
+  names(gent_cat_colors) <- gent_cat
 
   race_short_colors <-
     c("#481567FF", "#33638DDF", "#FDE725FF", "#20A387FF")
   race_short <- c("Predominantly Black", "Black-Other", "White/White-Mixed", "Multiethnic/Other")
+  names(race_short_colors) <- race_short
 
   inc_cat_colors <-
     c("#c7cff2","#8897db","#697fe0","#4c66d9","#1437cc")
   inc_cat <- c("Bottom Quintile", "Second Quintile", "Middle Quintile", "Fourth Quintile", "Top Quintile")
+  names(inc_cat_colors) <- inc_cat
 
   labels = c("Overall", gent_cat, race_short, inc_cat)
   colors = c("white", gent_cat_colors, race_short_colors, inc_cat_colors)
+
+  data$ses <- factor(data$ses,
+                     levels = c("Low",
+                                "Moderate",
+                                "Middle",
+                                "High"))
 
   if (scale_type == "percent") {
     label_type = scales::percent
@@ -80,7 +90,6 @@ plot_lollipop <- function(
     geom_point(aes(color = factor(cat)), size = 3.25, shape = 21,
                colour = "black",show.legend = TRUE) +
     geom_hline(yintercept=0, linetype="dashed") +
-    facet_grid(rows = vars(facet), scale = "free", space = "free") +
     scale_y_continuous(limits = limits,
                        expand = c(0, 0),
                        labels = label_type) +
@@ -88,28 +97,43 @@ plot_lollipop <- function(
                        labels = labels) +
     scale_fill_manual(values = colors) +
     theme_bw() +
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.background = element_blank()
-    ) +
-    theme(axis.line = element_line(colour = "black"),
-          axis.text.x = element_text(angle = 45, hjust = 1)) +
-    theme(legend.position = "none") +
+    theme(
+      # Panel
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_blank(),
+      panel.spacing.x = unit(1, "lines"),
+      # Axis
+      axis.line = element_line(colour = "black"),
+      axis.text.x = element_text(angle = 45, hjust = 1),
+      axis.title = element_blank(),
+      axis.title.x = element_text(size = 9),
+      # Legend
+      legend.position = "none",
+      # Caption
+      plot.caption = element_text(size = 6, hjust = .5, face = "italic")) +
     labs(y = x_title, caption = caption) +
-    theme(plot.caption = element_text(size = 8, hjust = .5, face = "italic"),
-          axis.title = element_blank(),
-          axis.title.x = element_text(size = 9)) +
     coord_flip()
-
-  if (reverse) {
+  if (ses) {
     plot = plot +
-      scale_y_reverse(limits = rev(limits),
-                      expand = c(0, 0),
-                      labels = label_type)
+      facet_grid(rows = vars(facet),
+                 cols = vars(ses),
+                 scale = "free",
+                 space = "free")
+    width = 6.8
+    height = 5.1
+
+  } else {
+    plot = plot +
+      facet_grid(rows = vars(facet),
+                 scale = "free",
+                 space = "free")
+    width = 5.5
+    height = 4.7
   }
 
   if (save) {
-    ggsave(savename, plot, height = 4.5, width = 4.5)
+    ggsave(savename, plot, height = height, width = width)
   }
   return(plot)
 
